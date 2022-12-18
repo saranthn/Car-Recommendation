f3b063789537ba3f7d0675b178d9adca63efc54c
Saravanan Thanu <saranthn@gmail.com>
2022-12-17 22:46:42 -0500

    Add model dropdown

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+// JavaScript
+
+const makeInput = document.getElementById('makeInput');
+var modelInput = document.getElementById('modelInput');
+var modelDatalist = document.getElementById('model');
+
+makeInput.addEventListener('change', e => {
+  // Enable the model input and year select element
+  modelInput.disabled = false;
+
+  // Clear the model input and year select element
+  modelDatalist.innerHTML = '';
+
+  // Fill the model input and year select element based on the selected make
+  fillModels(e.target.value);
+});
+
+function fillModels(make) {
+  // Get the list of models for the selected make
+  var models = getModelsForMake(make);
+
+  if (models === null || models.length === 0) {
+  	modelInput.disabled = true;
+  }
+
+  // Loop through the models and add an option element for each model
+  models.forEach(model => {
+    var option = document.createElement('option');
+    option.setAttribute('value', model);
+    option.innerHTML = model;
+    modelDatalist.appendChild(option);
+  });
+}
+
+function getModelsForMake(make) {
+  // Return the list of models for the selected make
+  // You can use a switch statement or an object with key-value pairs to map make to models
+  // For example:
+  switch (make) {
+    case 'BMW':
+      return ['X1', 'X3', 'X5'];
+    case 'Audi':
+      return ['A3', 'A4', 'A6'];
+    // Add case statements for other makes
+    default:
+      return [];
+  }
+}
+
 function loadHomePage() {
 	const list = "BMW, Audi, FIAT, Mercedes-Benz, Chrysler, Nissan, Volvo, Mazda, Mitsubishi, Ferrari, Alfa Romeo, Toyota, McLaren, Maybach, Pontiac, Porsche, Saab, GMC, Hyundai, Plymouth, Honda, Oldsmobile, Suzuki, Ford, Cadillac, Kia, Bentley, Chevrolet, Dodge, Lamborghini, Lincoln, Subaru, Volkswagen, Spyker, Buick, Acura, Rolls-Royce, Maserati, Lexus, Aston Martin, Land Rover, Lotus, Infiniti, Scion, Genesis, HUMMER, Tesla, Bugatti";
 	const carList = list.split(', ');
