68ee25f2866e67c6773ddd799786462f78824dc4
Saravanan Thanu <saranthn@gmail.com>
2022-12-18 22:34:18 -0500

    remove redundant code

--- main.js
+++ main.js
@@ -412,11 +412,11 @@ function login() {
     }
 }
 
-const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
-AWS.config.region = 'us-east-1';
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1_DirUZXt2s'
-});
+// const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+// AWS.config.region = 'us-east-1';
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: 'us-east-1_DirUZXt2s'
+// });
 
 
 const button = document.getElementById('sign-out-button');
