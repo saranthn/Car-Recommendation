140ba0568543db5201d7b0dc44d8a692d7aca09a
Atul Balaji <atul@Atuls-Air.lan>
2022-12-20 16:11:20 -0500

    Test16

--- main.js
+++ main.js
@@ -401,7 +401,7 @@ function login() {
 
 const button = document.getElementById('sign-out-button');
 button.addEventListener('click', async function() {
-	window.location.replace('https://carfinder.auth.us-east-1.amazoncognito.com/logout?client_id=3ibqh30l0u9h5n3n3n56q0d6q0&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d19fq1agwipela.cloudfront.net/index.html');
+	window.location.replace('https://carfinder.auth.us-east-1.amazoncognito.com/logout?client_id=3ibqh30l0u9h5n3n3n56q0d6q0&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d19fq1agwipela.cloudfront.net/index.html');
 });
 
 
@@ -475,3 +475,4 @@ function toggleIcon(x, carid) {
 
 	//TODO: PUT request to update items in wishlist or favorites
 }
+
